7a504d6a9e10f27b8fac10444cf8ebb277a71c42
c-1-2 <daphinekhasiro@gmail.com>
2024-04-13 14:35:53 +0300

    first commit

---
+++ hello.js
@@ -0,0 +1,12 @@
+console.log("Hello, my name is Daphine.");
+console.log("I am a student.");
+console.log("We are learning JavaScript.");
+
+var name= "Daphine Khasiro"
+console.log (name);
+
+let message= "I am a student at PLP";
+console.log(message);
+
+const PI=3.1452;
+console.log (PI);
